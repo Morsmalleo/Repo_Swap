e8eb2c333dabaf79d7336b0bbf15f467f0357a3e
Morsmalleo <64344168+Morsmalleo@users.noreply.github.com>
2021-09-15 00:07:06 -0400

    Modified repo_swap.sh

--- repo_swap.sh
+++ repo_swap.sh
@@ -16,15 +16,19 @@ touch /etc/apt/sources.list.d/<insert name>.list
 echo "deb http://<input software repo here>" > /etc/apt/sources.list.d/<insert name>.list
 gnome-terminal -t " Updating Repositories <input software repo name here> " -geometry 100x30 -- "apt-get update -y"
 sleep 1
-
-	apt-get install whatever package you need to install from an older software repo
+        # installs an older package from the older software repo added by the code above
+	apt-get install <older package name>
 
 echo " Removing Repositories <input software repo name here> " rm -f /etc/apt/sources.list.d/<insert name>.list
 #now we can remove the emergency backup securely
 gnome-terminal -t " Updating Your Current Repositories List(s) " -geometry 100x30 -- "apt-get update"
 sleep 1
    
-    apt-get install <package name>
+   # instals a more recent package from your original
+   # software repositories list (sources.list)
+   # once the older package has been installed
+   # and the older software repo .list file has been removed
+    apt-get install <upto date package name>
     
 echo " cleaning your cache "
 apt-get clean && apt-get clean cache && apt-get autoclean
